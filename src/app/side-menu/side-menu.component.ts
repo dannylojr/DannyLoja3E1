43b904b52c1fa2a68afc6fe152a909911f2afe09
dannyloja145 <dannyloja145@gmail.com>
2024-09-18 16:50:58 +0000

    first-commit

---
+++ src/app/side-menu/side-menu.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-side-menu',
+  standalone: true,
+  imports: [],
+  templateUrl: './side-menu.component.html',
+  styleUrl: './side-menu.component.css'
+})
+export class SideMenuComponent {
+  userName: string = "Danny Loja";
+  theme: string = "";
+  widthMenu: number = 50;
+  visibleMenu: boolean = true;
+
+  setVisibilityBack():void {
+    this.visibleMenu = !this.visibleMenu;
+    this.visibleMenu = false;
+  }
+  setVisibilityMenu():void {
+    this.visibleMenu = !this.visibleMenu;
+    this.visibleMenu = true;
+  }
+
+  themeColors = ['rgb(19, 90, 114)', '#ff6363', '#ffc857', '#6a00f4'];
+  currentColorIndex = 0;
+  setThemeColor(color: string):void{
+    this.currentColorIndex = (this.currentColorIndex + 1) % this.themeColors.length;
+    this.theme = this.themeColors[this.currentColorIndex];
+  }
+
+  setSizeMenu(){
+    this.widthMenu+=10;
+    if(this.widthMenu>100){
+      this.widthMenu = 50;
+    }
+  }
+}
